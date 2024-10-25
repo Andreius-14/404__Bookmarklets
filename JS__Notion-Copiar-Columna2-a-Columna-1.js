70e7428b48a8967816521fa2ddff21ab66c3e965
Andreius_14 <Github_404@outlook.com>
2024-10-24 23:16:07 -0500

    Basura

--- JS__Notion-Copiar-Columna2-a-Columna-1.js
+++ JS__Notion-Copiar-Columna2-a-Columna-1.js
@@ -1,16 +1,25 @@
-let rows = document.querySelectorAll(".notion-selectable.notion-page-block.notion-collection-item");
+const rows = document.querySelectorAll('.notion-selectable.notion-page-block.notion-collection-item')
 
-rows.forEach((row) => {
+rows.forEach((row, indice) => {
   // Seleccionar el contenido de la columna 2
-  let column2 = row.querySelector('[data-col-index="1"] div div span');
+  const column2 = row.querySelector('[data-col-index="1"] div div span span')
   if (column2) {
-    let column2Text = column2.innerText;
-
     // Seleccionar la columna 1
-    let column1 = row.querySelector('[data-col-index="0"] div div span');
-    if (column1) {
-      // Copiar el contenido de la columna 2 a la columna 1
-      column1.innerText = column2Text;
+    const column1 = row.querySelector('[data-col-index="0"] div div span')
+    if (!column1.hasChildNodes()) {
+      setTimeout(() => {
+        column1.click()
+        column1.value = column2.innerText
+      }, 1500 * indice)
     }
   }
-});
+})
+
+
+const rows = document.querySelectorAll('.notion-selectable.notion-page-block.notion-collection-item')
+rows.forEach((row, indice) => {
+
+  const column1 = row.querySelector('[data-col-index="0"] div div span')
+  const column2 = row.querySelector('[data-col-index="1"] div div span span')
+
+})
