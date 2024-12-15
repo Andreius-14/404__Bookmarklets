1f3fb8d415724f849472d7faa54d47d0f6f320f4
Andreius_14 <Github_404@outlook.com>
2024-12-15 02:17:11 -0500

    F11_2

--- Bookmarklets__F11.js
+++ Bookmarklets__F11.js
@@ -1,13 +1,10 @@
 (function() {
     function toggleFullScreen() {
-        // Verifica si hay un elemento en pantalla completa
         if (document.fullscreenElement) {
-            // Si hay un elemento en pantalla completa, salimos de modo pantalla completa
             document.exitFullscreen && document.exitFullscreen();
         } else {
-            // Si no hay un elemento en pantalla completa, solicitamos pantalla completa
             document.documentElement.requestFullscreen();
         }
     }
-    toggleFullScreen(); // Llama a la función para alternar el estado
+    toggleFullScreen(); 
 }());
